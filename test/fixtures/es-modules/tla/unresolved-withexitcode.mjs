549873d5ce06c6519a2cc92a977f6031f28b43ae
Dario Piotrowicz <dario@cloudflare.com>
2025-02-04 21:30:27 +0000

    fixup! src: fix process exit listeners not receiving unsettled tla codes
    split tests

--- test/fixtures/es-modules/tla/unresolved-withexitcode.mjs
+++ test/fixtures/es-modules/tla/unresolved-withexitcode.mjs
@@ -1,7 +1,2 @@
-process.on('exit', (exitCode) => {
-    console.log(`the exit listener received code: ${exitCode}`);
-});
-
 process.exitCode = 42;
-
 await new Promise(() => {});
--- test/fixtures/es-modules/tla/unresolved.mjs
+++ test/fixtures/es-modules/tla/unresolved.mjs
@@ -1,5 +1,1 @@
-process.on('exit', (exitCode) => {
-    console.log(`the exit listener received code: ${exitCode}`);
-})
-
 await new Promise(() => {});
