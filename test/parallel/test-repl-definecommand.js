1152fd3eb6b426c1ee37a9da6ab5647afced0439
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2025-07-29 01:16:36 +0100

    apply lint issues that are all auto-fixable

--- test/parallel/test-repl-definecommand.js
+++ test/parallel/test-repl-definecommand.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
-require('../common');
+require("../common");
 
-const stream = require('stream');
-const assert = require('assert');
-const repl = require('repl');
+const stream = require("stream");
+const assert = require("assert");
+const repl = require("repl");
 
-let output = '';
+let output = "";
 const inputStream = new stream.PassThrough();
 const outputStream = new stream.PassThrough();
-outputStream.on('data', function(d) {
+outputStream.on("data", function(d) {
   output += d;
 });
 
@@ -19,31 +19,31 @@ const r = repl.start({
   terminal: true
 });
 
-r.defineCommand('say1', {
-  help: 'help for say1',
+r.defineCommand("say1", {
+  help: "help for say1",
   action: function(thing) {
-    output = '';
+    output = "";
     this.output.write(`hello ${thing}\n`);
     this.displayPrompt();
   }
 });
 
-r.defineCommand('say2', function() {
-  output = '';
-  this.output.write('hello from say2\n');
+r.defineCommand("say2", function() {
+  output = "";
+  this.output.write("hello from say2\n");
   this.displayPrompt();
 });
 
-inputStream.write('.help\n');
+inputStream.write(".help\n");
 assert.match(output, /\n\.say1 {5}help for say1\n/);
 assert.match(output, /\n\.say2\n/);
-inputStream.write('.say1 node developer\n');
-assert.ok(output.startsWith('hello node developer\n'),
+inputStream.write(".say1 node developer\n");
+assert.ok(output.startsWith("hello node developer\n"),
           `say1 output starts incorrectly: "${output}"`);
-assert.ok(output.includes('> '),
+assert.ok(output.includes("> "),
           `say1 output does not include prompt: "${output}"`);
-inputStream.write('.say2 node developer\n');
-assert.ok(output.startsWith('hello from say2\n'),
+inputStream.write(".say2 node developer\n");
+assert.ok(output.startsWith("hello from say2\n"),
           `say2 output starts incorrectly: "${output}"`);
-assert.ok(output.includes('> '),
+assert.ok(output.includes("> "),
           `say2 output does not include prompt: "${output}"`);
